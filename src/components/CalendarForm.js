a098032389aebe9a530b6b533c4ad074df0aeaef
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-12-03 00:32:55 +0100

    feat: add controlled form

--- src/components/CalendarForm.js
+++ src/components/CalendarForm.js
@@ -1,6 +1,31 @@
 import React from 'react';
 
 export default class CalendarForm extends React.Component {
+    state = {
+        name: '',
+        lastName: '',
+        email: '',
+        date: '',
+        time: '',
+        errors: []
+    };
+
+    inputChange = e => {
+        console.log(e.target.name);
+        console.log(e.target.value);
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value,
+        });
+    };
+
+    submitHandler = e => {
+        e.preventDefault();
+        const { name, lastName, email, date, time } = this.state;
+        console.log(name, lastName, email, date, time);
+        console.log('data: ' + date);
+    };
+
     renderFormInputs() {
         const { fields } = this.props;
         return (
@@ -10,7 +35,14 @@ export default class CalendarForm extends React.Component {
                     <div className='form__field' key={index}>
                         <label className='form__label' htmlFor={name}>
                             {label}
-                            <input className='form__input' id={name} name={name} type={type} />
+                            <input
+                                className='form__input'
+                                id={name}
+                                name={name}
+                                type={type}
+                                value={this.state[name]}
+                                onChange={this.inputChange}
+                            />
                             <span className='form__border'></span>
                         </label>
                     </div>
@@ -27,10 +59,14 @@ export default class CalendarForm extends React.Component {
                     <p className='calendar__description'>time is money</p>
                 </header>
                 <div className='calendar__container'>
-                    <form className='calendar__form form'>
+                    <form
+                        className='calendar__form form'
+                        onSubmit={this.submitHandler}
+                        noValidate
+                    >
                         {this.renderFormInputs()}
                         <div className='form__field'>
-                            <input className='form__submit' value='Add' type='submit'/>
+                            <input className='form__submit' value='Add' type='submit' />
                         </div>
                     </form>
                 </div>
