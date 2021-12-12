51b1cf0093bfc5dc424c6fa24e0e87729798f146
Szymon Suchanowski <szymonsuchanowski@gmail.com>
2021-12-13 00:22:01 +0100

    refactor: code structure

--- src/components/CalendarForm.js
+++ src/components/CalendarForm.js
@@ -48,7 +48,7 @@ export default class CalendarForm extends React.Component {
                         >
                             {label}
                             <input
-                                className={this.setInputClass(name)}
+                                className={this.setInputClassName(name)}
                                 id={name}
                                 name={name}
                                 type={type}
@@ -57,7 +57,7 @@ export default class CalendarForm extends React.Component {
                                 min={this.setDateRange(type)}
                                 autoComplete='off'
                             />
-                            <span className={this.setBorderClass(name)}></span>
+                            <span className={this.setBorderClassName(name)}></span>
                         </label>
                         {this.renderSuggestions(name)}
                         <div className='form__placeholder'>
@@ -69,87 +69,13 @@ export default class CalendarForm extends React.Component {
         );
     };
 
-    setInputClass(inputName) {
-        return this.state[inputName].isValid ? 'form__input' : 'form__input form__input--invalid';
-    };
-
-    setBorderClass(inputName) {
-        return this.state[inputName].isValid ? 'form__border' : 'form__border form__border--invalid';
-    };
-
-    setDateRange(type) {
-        return type === 'date' ? this.getCurrentDate() : null;
-    };
-
-    handleFocus = (inputName, filter) => {
-        const newStateData = {
-            showSuggestions: true,
-        };
-        return filter ? this.updateInputState(inputName, newStateData) : null;
-    };
-
-    handleBlur = (inputName, filter) => {
-        if (filter) {
-            setTimeout(() => {
-                this.updateInputState(inputName, { showSuggestions: false });
-            }, 200);
-        };
-        return null;
-    };
-
     renderSuggestions(name) {
         return this.isSuggestion(name) ? this.renderSuggestionsList(name) : null;
     };
 
-    isSuggestion(inputName) {
-        const { showSuggestions, dataSuggestions, value } = this.state[inputName];
-        return (showSuggestions && !this.isArrayEmpty(dataSuggestions) && value.length > 1);
-    };
-
-    renderSuggestionsList(inputName) {
-        const suggestionContentList = this.getSuggestionContentList(inputName);
-        const suggestionsItemList = this.renderSuggestionItem(suggestionContentList, inputName);
-        return (
-            <ul className='form__prompts'>
-                {suggestionsItemList}
-            </ul>
-        );
-    };
-
-    getSuggestionContentList(inputName) {
-        return this.state[inputName].dataSuggestions.map(suggestion => suggestion[inputName]);
-    };
-
-    completeField = (e, inputName) => {
-        const value = e.target.innerText;
-        const newStateData = {
-            value,
-            dataSuggestions: [],
-        };
-        this.updateInputState(inputName, newStateData);
-    };
-
-    renderSuggestionItem(suggestionContentList, inputName) {
-        return suggestionContentList.map((suggestionItem, index) =>
-            <li className='form__suggestion'
-                onClick={e => this.completeField(e, inputName)}
-                key={index}
-            >
-                {suggestionItem}
-            </li>
-        );
-    };
-
     renderFormErrMsg(name) {
-        if (!this.isObjectEmpty(this.state.errors) && this.state.errors[name]) {
-            const errMsg = this.state.errors[name];
-            return (
-                <p className='form__err'>
-                    {errMsg}
-                </p>
-            );
-        }
-        return null;
+        return (!this.isObjectEmpty(this.state.errors) && this.state.errors[name]) ?
+            this.showErrMsg(name) : null;
     };
 
     createStateObject() {
@@ -183,8 +109,71 @@ export default class CalendarForm extends React.Component {
 
     submitHandler = e => {
         e.preventDefault();
-        const errors = this.checkDataCorrectness();
+        const errors = this.findErrors();
         return this.isObjectEmpty(errors) ? this.addNewMeeting() : this.showErrors(errors);
+    };
+
+    findErrors() {
+        const errorsArr = [];
+        this.createErrors(errorsArr);
+        const errorsObj = this.convertArrToObj(errorsArr);
+        return errorsObj;
+    };
+
+    createErrors(errorsArr) {
+        const validator = new DataValidator();
+        const inputsNamesList = this.getInputsNames();
+        inputsNamesList.forEach(inputName => {
+            const err = validator.checkDataErrors(inputName, this.state[inputName].value);
+            return err ? this.setInputInvalid(errorsArr, err, inputName) : this.setInputValid(inputName);
+        });
+    };
+
+    setInputInvalid(errors, err, inputName) {
+        errors.push(err);
+        const newStateData = {
+            isValid: false,
+        };
+        this.updateInputState(inputName, newStateData);
+    };
+
+    setInputValid(inputName) {
+        if (!this.state[inputName].isValid) {
+            const newStateData = {
+                isValid: true,
+            }
+            this.updateInputState(inputName, newStateData);
+        };
+    };
+
+    addNewMeeting() {
+        const inputValuesList = this.getInputValues();
+        const meetingData = this.convertArrToObj(inputValuesList);
+        this.props.addMeeting(meetingData);
+        const stateObject = this.createStateObject();
+        this.setState(stateObject);
+    };
+
+    showErrors(errors) {
+        this.setState({
+            errors: errors
+        });
+    };
+
+    handleFocus = (inputName, filter) => {
+        const newStateData = {
+            showSuggestions: true,
+        };
+        return filter ? this.updateInputState(inputName, newStateData) : null;
+    };
+
+    handleBlur = (inputName, filter) => {
+        if (filter) {
+            setTimeout(() => {
+                this.updateInputState(inputName, { showSuggestions: false });
+            }, 200);
+        };
+        return null;
     };
 
     inputChange = e => {
@@ -215,47 +204,58 @@ export default class CalendarForm extends React.Component {
         this.updateInputState(inputName, newStateData);
     };
 
-    checkDataCorrectness() {
-        const errorsArr = [];
-        this.createErrors(errorsArr);
-        const errorsObj = this.convertArrToObj(errorsArr);
-        return errorsObj;
+    isSuggestion(inputName) {
+        const { showSuggestions, dataSuggestions, value } = this.state[inputName];
+        return (showSuggestions && !this.isArrayEmpty(dataSuggestions) && value.length > 1);
     };
 
-    createErrors(errorsArr) {
-        const validator = new DataValidator();
-        const inputsNamesList = this.getInputsNames();
-        inputsNamesList.forEach(inputName => {
-            const err = validator.checkDataErrors(inputName, this.state[inputName].value);
-            if (err) {
-                errorsArr.push(err);
-                const newStateData = {
-                    isValid: false,
-                }
-                this.updateInputState(inputName, newStateData);
-            } else {
-                if (!this.state[inputName].isValid) {
-                    const newStateData = {
-                        isValid: true,
-                    }
-                    this.updateInputState(inputName, newStateData);
-                }
-            };
-        });
+    renderSuggestionsList(inputName) {
+        const suggestionContentList = this.getSuggestionContentList(inputName);
+        const suggestionsItemList = this.renderSuggestionItem(suggestionContentList, inputName);
+        return (
+            <ul className='form__prompts'>
+                {suggestionsItemList}
+            </ul>
+        );
     };
 
-    addNewMeeting() {
-        const inputValuesList = this.getInputValues();
-        const meetingData = this.convertArrToObj(inputValuesList);
-        this.props.addMeeting(meetingData);
-        const stateObject = this.createStateObject();
-        this.setState(stateObject);
+    getSuggestionContentList(inputName) {
+        return this.state[inputName].dataSuggestions.map(suggestion => suggestion[inputName]);
     };
 
-    showErrors(errors) {
-        this.setState({
-            errors: errors
-        });
+    renderSuggestionItem(suggestionContentList, inputName) {
+        return suggestionContentList.map((suggestionItem, index) =>
+            <li className='form__suggestion'
+                onClick={e => this.completeField(e, inputName)}
+                key={index}
+            >
+                {suggestionItem}
+            </li>
+        );
+    };
+
+    completeField = (e, inputName) => {
+        const value = e.target.innerText;
+        const newStateData = {
+            value,
+            dataSuggestions: [],
+        };
+        this.updateInputState(inputName, newStateData);
+    };
+
+    showErrMsg(inputName) {
+        const errMsg = this.state.errors[inputName];
+        return (
+            <p className='form__err'>
+                {errMsg}
+            </p>
+        );
+    };
+
+    updateInputState(inputName, newStateData) {
+        this.setState(prevState => ({
+            [inputName]: { ...prevState[inputName], ...newStateData },
+        }));
     };
 
     getInputValues() {
@@ -270,12 +270,6 @@ export default class CalendarForm extends React.Component {
         return fields.map(input => input.name);
     };
 
-    updateInputState(inputName, newStateData) {
-        this.setState(prevState => ({
-            [inputName]: { ...prevState[inputName], ...newStateData },
-        }));
-    };
-
     convertArrToObj(arr) {
         return Object.assign({}, ...arr);
     };
@@ -288,8 +282,20 @@ export default class CalendarForm extends React.Component {
         return arr.length === 0;
     };
 
+    setDateRange(type) {
+        return type === 'date' ? this.getCurrentDate() : null;
+    };
+
     getCurrentDate() {
         const timezoneOffset = (new Date()).getTimezoneOffset() * 60000;
         return (new Date(Date.now() - timezoneOffset)).toISOString().slice(0, 10);
+    };
+
+    setInputClassName(inputName) {
+        return this.state[inputName].isValid ? 'form__input' : 'form__input form__input--invalid';
+    };
+
+    setBorderClassName(inputName) {
+        return this.state[inputName].isValid ? 'form__border' : 'form__border form__border--invalid';
     };
 }
